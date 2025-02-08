49bc9735dc8fde1c527b3bddcad7899a7ff2e7a0
Fi-Boo <s2008156@student.rmit.edu.au>
2025-02-09 10:01:11 +1100

    carousel working

--- js/main.js
+++ js/main.js
@@ -27,3 +27,29 @@ document.addEventListener("DOMContentLoaded", function() {
     })
 });
 
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '',
+    },
+
+    autoplay: {
+        delay: 7000,
+    },
+});
